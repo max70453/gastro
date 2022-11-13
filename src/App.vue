d84459ebf9b1d4f6c6b6f767245d342f092c0368
max70453 <max70453@gmail.com>
2022-11-13 23:46:50 +0300

    nav menu and phone number

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,4 @@ export default {
 
 <style lang="sass">
 @import './styles/index.scss'
-#app 
-  font-family: $font-family-base, Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-  text-align: center
-  color: #2c3e50
-  margin-top: 60px
-
 </style>
--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -1,45 +1,79 @@
 <template lang='pug'>
-div
-  h1 {{ msg }}
-
-  h3 navigate
-  ul
-    li
-      <router-link to="/Nutrition-Programm">Программа питания</router-link>
-    li
-      <router-link to="/Business-Lanchess">Бизнес ланчи</router-link>
-    li
-      <router-link to="/Gastro-Shop">GastroShop</router-link>
-    li
-      <router-link to="/about">О нас</router-link>
-    li
-      <router-link to="/blog">Блог</router-link>
+.inner-container
+  nav.nav
+    ul.nav__list
+      li(v-for="link, index in navItems", :key="index").nav__listItem
+        router-link(:to="link.path").nav__linkItem {{link.text}}
+  .nav__phone
+    .nav__phoneNumber {{ number }}
 </template>
 
 <script>
 export default {
   name: 'HomePage',
-  props: {
-    msg: String
+  data() {
+    return {
+      navItems: [
+        {path: '/Nutrition-Programm', text: 'Программа питания'},
+        {path: '/Business-Lanchess', text: 'Бизнес-ланчи'},
+        {path: '/Gastro-Shop', text: 'Gastro Shop'},
+        {path: '/about', text: 'О нас'},
+        {path: '/blog', text: 'Блог'},
+      ],
+      number: '+38 (068) 949 - 49 -19'
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="sass" scoped>
-// @import "../variables.scss"
+<style lang="sass">
 @import '../../styles/index.scss'
-h3
-  margin: 40px 0 0
-ul
-  list-style-type: none
-  padding: 0
-li
+
+.inner-container
+  width: 100%
+  margin: 0 auto
+  max-width: 1287px
+  display: flex
+
+.nav
+  width: 75%
+  text-align: end
+  padding: 17px 40px
+  background: $color-background-base
+  box-shadow: 0px 0px 40px rgba(86, 86, 86, 0.3)
+  border-radius: 190px
+  margin-right: 20px
+
+.nav__list
+  margin: 0
+
+.nav__listItem
+  font-family: $font-family-base
+  font-style: normal
+  font-weight: 400
+  font-size: 14px
+  line-height: 133.8%
+  color: $base-text-color
   display: inline-block
-  margin: 0 10px
-a
-  color: $error-color
-// h1
-//   font-family: "Montserrat-Regular", sans-serif
+  margin-right: 40px
+  &:last-child
+    margin-right: 0
+
+.nav__linkItem
+  &:hover
+    color: $base-btn-color
+  &:active
+    color: $active-color-link
+
+.nav__phone
+  padding: 15px 30px
+  background: $color-background-base
+  box-shadow: 0px 0px 40px rgba(86, 86, 86, 0.3)
+  border-radius: 190px
+  align-self: center
+
+.nav__phoneNumber
+  color: $base-btn-color
+  font-family: $font-family-bold
 
 </style>
